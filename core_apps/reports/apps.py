947d9bf29df32d2ce31786c32d2581616f5085c5
danidobos <danidobos97@gmail.com>
2024-11-12 13:54:19 +0100

    Create reports signals

--- core_apps/reports/apps.py
+++ core_apps/reports/apps.py
@@ -6,3 +6,6 @@ class ReportsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core_apps.reports"
     verbose_name = _('Reports')
+
+    def ready(self) -> None:
+        import core_apps.reports.signals
