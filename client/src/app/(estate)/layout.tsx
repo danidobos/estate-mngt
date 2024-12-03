c4f3208c9f81bf8a24080ecec5abf578b203b9fd
danidobos <danidobos97@gmail.com>
2024-12-03 18:07:01 +0100

    feat: add left navigation sidebar

--- client/src/app/(estate)/layout.tsx
+++ client/src/app/(estate)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "@/components/shared/navbar/Navbar";
+import LeftNavbar from "@/components/shared/navbar/LeftNavbar";
 
 interface LayoutProps {
 	children: React.ReactNode;
@@ -10,10 +11,8 @@ export default function layout({ children }: LayoutProps) {
 		<main className="bg-baby_veryBlack relative">
 			<Navbar />
 			<div className="flex">
-				{/* placeholder left navbar component */}
-				<div className="dark:text-pumpkin hidden text-xl md:block">
-					Left Navbar
-				</div>
+				<LeftNavbar />
+
 				<section className="flex min-h-screen flex-1 flex-col px-4 pb-6 pt-24 sm:px-6 lg:px-8 lg:pt-32">
 					<div>{children}</div>
 				</section>
