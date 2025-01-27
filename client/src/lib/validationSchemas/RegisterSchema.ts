d67f554e28fcd2ed741e6c38dc2f78d3003ed0eb
danidobos <danidobos97@gmail.com>
2025-01-27 16:28:03 +0100

    chore: setup zod validation

---
+++ client/src/lib/validationSchemas/RegisterSchema.ts
@@ -0,0 +1,38 @@
+import * as z from "zod";
+
+const usernameRegex = /^[a-zA-z0-9_@+.-]+$/;
+
+export const registerUserSchema = z
+	.object({
+		username: z.string().regex(usernameRegex, {
+			message:
+				"Usernames can only contain letters (uppercase and lowercase), digits, _, @, +, . and -",
+		}),
+		first_name: z
+			.string()
+			.trim()
+			.min(2, { message: "First name must be at least 2 characters long." })
+			.max(50, {
+				message: "First name must be at less than 50 characters long.",
+			}),
+		last_name: z
+			.string()
+			.trim()
+			.min(2, { message: "Last name must be at least 2 characters long." })
+			.max(50, {
+				message: "Last name must be at less than 50 characters long.",
+			}),
+		email: z.string().trim().email({ message: "Enter a valid email address." }),
+		password: z
+			.string()
+			.min(8, { message: "Password must be at least 8 characters long." }),
+		re_password: z.string().min(8, {
+			message: "Confirm password must be at least 8 characters long.",
+		}),
+	})
+	.refine((data) => data.password === data.re_password, {
+		message: "Passwords do mot match",
+		path: ["re_password"],
+	});
+
+export type TRegisterUserSchema = z.infer<typeof registerUserSchema>;
