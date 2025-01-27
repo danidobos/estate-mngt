74a580c2d3fa31cf8074ec6f806372c5508eff2e
danidobos <danidobos97@gmail.com>
2025-01-27 16:45:29 +0100

    feat: add Auth form header component

---
+++ client/src/app/(estate)/(auth)/activate/[uid]/[token]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ActivationPage() {
+	return (
+		<div>
+			<h1 className="dark:text-pumpkin text-6xl">Activation Page</h1>
+		</div>
+	);
+}
--- client/src/app/(estate)/(auth)/register/page.tsx
+++ client/src/app/(estate)/(auth)/register/page.tsx
@@ -1,9 +1,15 @@
+import { AuthFormHeader } from "@/components/forms/auth";
 import React from "react";
 
 export default function RegisterPage() {
 	return (
 		<div>
-			<h1 className="dark:text-pumpkin text-6xl">Register Page</h1>
+			<AuthFormHeader
+				title="Sign up for an account"
+				staticText="Already have an account?"
+				linkText="Login Here"
+				linkHref="/login"
+			/>
 		</div>
 	);
 }
