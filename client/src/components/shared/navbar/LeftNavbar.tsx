c87092cafc3fc52296cb4d8ef04d8448c21a4209
danidobos <danidobos97@gmail.com>
2025-01-29 12:43:09 +0100

    feat: persist auth, filter auth links in left navbar

--- client/src/components/shared/navbar/LeftNavbar.tsx
+++ client/src/components/shared/navbar/LeftNavbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { leftNavLinks } from "@/constants";
+import { useAuthNavigation } from "@/hooks";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -8,11 +9,13 @@ import React from "react";
 
 export default function LeftNavbar() {
 	const pathname = usePathname();
+	const { handleLogout, filteredNavLinks, isAuthenticated } =
+		useAuthNavigation();
 
 	return (
 		<section className="bg-baby_rich light-border custom-scrollbar shadow-platinum sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 max-sm:hidden lg:w-[297px] dark:shadow-none">
 			<div className="flex flex-1 flex-col gap-6">
-				{leftNavLinks.map((linkItem) => {
+				{filteredNavLinks.map((linkItem) => {
 					const isActive =
 						(pathname.includes(linkItem.path) && linkItem.path.length > 1) ||
 						pathname === linkItem.path;
@@ -40,19 +43,30 @@ export default function LeftNavbar() {
 				})}
 			</div>
 
-			<div className="flex flex-col gap-3">
-				<Link href="/login">
-					<Button className="lime-gradient small-medium light-border-2 btn-tertiary text-baby_ballon min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-						Login
+			{isAuthenticated ? (
+				<div>
+					<Button
+						onClick={handleLogout}
+						className="lime-gradient small-medium light-border-2 btn-tertiary text-baby_ballon min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none"
+					>
+						Log Out
 					</Button>
-				</Link>
+				</div>
+			) : (
+				<div className="flex flex-col gap-3">
+					<Link href="/login">
+						<Button className="lime-gradient small-medium light-border-2 btn-tertiary text-baby_ballon min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+							Login
+						</Button>
+					</Link>
 
-				<Link href="/register">
-					<Button className="electricIndigo-gradient small-medium light-border-2 btn-tertiary text-baby_ballon mt-4 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-						Register
-					</Button>
-				</Link>
-			</div>
+					<Link href="/register">
+						<Button className="electricIndigo-gradient small-medium light-border-2 btn-tertiary text-baby_ballon mt-4 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+							Register
+						</Button>
+					</Link>
+				</div>
+			)}
 		</section>
 	);
 }
